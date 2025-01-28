413e30b0eb90b4534350d6cbc62b6e15ca8d26c4
LedSquare <exilelovich@yandex.ru>
2025-01-28 11:23:39 +0300

    init

---
+++ src/InternalProtocolServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace InternalProtocol;
+
+use Illuminate\Support\ServiceProvider;
+
+class InternalProtocolServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+    }
+}
